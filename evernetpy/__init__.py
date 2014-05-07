74a7846f5b50971a109fa8ba9b1c5c01666a9979
eldang <eldang@gmail.com>
2014-05-07 15:39:26 -0700

    Extending area selection to all methods

--- evernetpy/__init__.py
+++ evernetpy/__init__.py
@@ -319,8 +319,8 @@ AREAS = [
 ]
 
 
-def get_all_listings(username, password, property_types=PROPERTY_TYPES):
-    return get_new_listings(username, password, hours_previous=876581, property_types=property_types)
+def get_all_listings(username, password, property_types=PROPERTY_TYPES, areas=AREAS):
+    return get_new_listings(username, password, hours_previous=876581, property_types=property_types, areas=AREAS)
 
 
 def get_all_active_mls_numbers(username, password, property_types=PROPERTY_TYPES, areas=AREAS):
@@ -335,14 +335,15 @@ def get_all_active_mls_numbers(username, password, property_types=PROPERTY_TYPES
                     yield info.get('LN')
 
 
-def get_new_listings(username, password, hours_previous=24, property_types=PROPERTY_TYPES):
+def get_new_listings(username, password, hours_previous=24, property_types=PROPERTY_TYPES, areas=AREAS):
     begin_date = (datetime.datetime.utcnow() - datetime.timedelta(hours=hours_previous)).strftime('%Y-%m-%dT%H:%M:%S')
     end_date = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
 
     for property_type in property_types:
-        for row in execute_listing_query(username, password, 'RetrieveListingData', {'BeginDate': begin_date, 'EndDate': end_date, 'MLS': 'nwmls', 'PropertyType': property_type}):
-            out = dict([(c.tag.replace('{http://www.nwmls.com/Schemas/Standard/StandardXML1_2.xsd}', ''), c.text) for c in row.getchildren()])
-            yield look_up_all_fields(username, password, out)
+        for area in areas:
+            for row in execute_listing_query(username, password, 'RetrieveListingData', {'BeginDate': begin_date, 'EndDate': end_date, 'MLS': 'nwmls', 'PropertyType': property_type, 'Area': area}):
+                out = dict([(c.tag.replace('{http://www.nwmls.com/Schemas/Standard/StandardXML1_2.xsd}', ''), c.text) for c in row.getchildren()])
+                yield look_up_all_fields(username, password, out)
 
 
 def get_photos(username, password, listing_id):

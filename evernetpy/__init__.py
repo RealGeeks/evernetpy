f4ee8bf0dfbaca00901bde81a787b2d423b78a03
eldang <eldang@gmail.com>
2014-05-08 15:25:53 -0700

    Avoids gratuitous looping through areas list
    If the areas argument is not provided to get_all_listings, get_all_active_mls_numbers or get_new_listings, they simply don't filter by area, as opposed to the previous version which filtered by every area in turn.

--- evernetpy/__init__.py
+++ evernetpy/__init__.py
@@ -2,7 +2,6 @@ import base64
 import datetime
 from execute import execute_listing_query, execute_photo_query
 from lookups import look_up_all_fields
-from lookups import FIXED_NWMLS_LOOKUPS
 
 PROPERTY_TYPES = [
     'BUSO',
@@ -18,34 +17,47 @@ PROPERTY_TYPES = [
     'VACL',
 ]
 
-AREAS = FIXED_NWMLS_LOOKUPS['SD'].values()
 
-
-def get_all_listings(username, password, property_types=PROPERTY_TYPES, areas=AREAS):
+def get_all_listings(username, password, property_types=PROPERTY_TYPES, areas=[]):
     return get_new_listings(username, password, hours_previous=876581, property_types=property_types, areas=areas)
 
 
-def get_all_active_mls_numbers(username, password, property_types=PROPERTY_TYPES, areas=AREAS):
+def get_all_active_mls_numbers(username, password, property_types=PROPERTY_TYPES, areas=[]):
     begin_date = "1990-01-01T00:00:00"
     end_date = "3000-01-01T00:00:00"
 
-    for property_type in property_types:
-        for area in areas:
-            for row in execute_listing_query(username, password, 'RetrieveListingData', {'BeginDate': begin_date, 'EndDate': end_date, 'MLS': 'nwmls', 'PropertyType': property_type, 'Area': area, 'Status': 'A'}, filter="LN,ST"):
+    if areas == []:
+        for property_type in property_types:
+            for row in execute_listing_query(username, password, 'RetrieveListingData', {'BeginDate': begin_date, 'EndDate': end_date, 'MLS': 'nwmls', 'PropertyType': property_type, 'Status': 'A'}, filter="LN,ST"):
                 info = dict([(c.tag.replace('{http://www.nwmls.com/Schemas/Standard/StandardXML1_2.xsd}', ''), c.text) for c in row.getchildren()])
                 if info.get('ST') == 'A':
                     yield info.get('LN')
+    else:
+        for property_type in property_types:
+            for area in areas:
+                for row in execute_listing_query(username, password, 'RetrieveListingData', {'BeginDate': begin_date, 'EndDate': end_date, 'MLS': 'nwmls', 'PropertyType': property_type, 'Area': area, 'Status': 'A'}, filter="LN,ST"):
+                    info = dict([(c.tag.replace('{http://www.nwmls.com/Schemas/Standard/StandardXML1_2.xsd}', ''), c.text) for c in row.getchildren()])
+                    if info.get('ST') == 'A':
+                        yield info.get('LN')
 
 
-def get_new_listings(username, password, hours_previous=24, property_types=PROPERTY_TYPES, areas=AREAS):
+
+def get_new_listings(username, password, hours_previous=24, property_types=PROPERTY_TYPES, areas=[]):
     begin_date = (datetime.datetime.utcnow() - datetime.timedelta(hours=hours_previous)).strftime('%Y-%m-%dT%H:%M:%S')
     end_date = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
 
-    for property_type in property_types:
-        for area in areas:
-            for row in execute_listing_query(username, password, 'RetrieveListingData', {'BeginDate': begin_date, 'EndDate': end_date, 'MLS': 'nwmls', 'PropertyType': property_type, 'Area': area}):
+    if areas == []:
+        for property_type in property_types:
+            for row in execute_listing_query(username, password, 'RetrieveListingData', {'BeginDate': begin_date, 'EndDate': end_date, 'MLS': 'nwmls', 'PropertyType': property_type}):
                 out = dict([(c.tag.replace('{http://www.nwmls.com/Schemas/Standard/StandardXML1_2.xsd}', ''), c.text) for c in row.getchildren()])
                 yield look_up_all_fields(username, password, out)
+    else:
+        for property_type in property_types:
+            for area in areas:
+                for row in execute_listing_query(username, password, 'RetrieveListingData', {'BeginDate': begin_date, 'EndDate': end_date, 'MLS': 'nwmls', 'PropertyType': property_type, 'Area': area}):
+                    out = dict([(c.tag.replace('{http://www.nwmls.com/Schemas/Standard/StandardXML1_2.xsd}', ''), c.text) for c in row.getchildren()])
+                    yield look_up_all_fields(username, password, out)
+
 
 
 def get_photos(username, password, listing_id):
